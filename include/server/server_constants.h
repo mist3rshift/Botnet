5cc908718c8e932fe82fa031d1ad0a6b9e5fc44d
Milan Soragna <milan.soragna@telecomnancy.eu>
2025-04-11 10:25:57 +0200

    [fix] Small deployment fixes

--- include/server/server_constants.h
+++ include/server/server_constants.h
@@ -8,7 +8,7 @@
 #define DEFAULT_SERVER_PORT "21000"
 #define DEFAULT_SERVER_ADDR "127.0.0.1"
 #define BACKUP_SERVER_ADDR "127.0.0.1"
-#define DEFAULT_WEB_ADDR "127.0.0.1"
+#define DEFAULT_WEB_ADDR "0.0.0.0" // Listen on all incoming
 #define DEFAULT_WEB_PORT "8000"
 #define MAX_CLIENTS 32
 
